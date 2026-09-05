e1cdba1013a591efbf0c227dfc64266fa6bcd8ec
VishalPanchal-01 <panchalvish001@gmail.com>
2026-09-05 10:46:41 +0530

    updated js file with deploy link

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "";
+  const API_BASE = "https://mentalhealthscorepredictor-pxdx.onrender.com";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
